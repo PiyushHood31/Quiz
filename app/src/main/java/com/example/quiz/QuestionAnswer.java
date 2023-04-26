da57a23919df664276367345d1a2dc41828db35e
PiyushHood31 <piyushhood31@gmail.com>
2023-04-26 12:22:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?",
+            "Which of the folloeing is not work of software engineer:"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"appDevlopment","webDevlopment","DataAnalysis","HR"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple",
+            "HR"
+    };
+
+}
